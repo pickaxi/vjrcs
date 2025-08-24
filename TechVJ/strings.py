646d3b1fbcd9cc60d3e210ad2e923e62865657b6
Sam <freestff0077@gmail.com>
2025-08-24 10:48:31 +0530

    Add files via upload

---
+++ TechVJ/strings.py
@@ -0,0 +1,46 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+strings = {
+    'need_login': "Sorry! Currently Private Chats Are Not Available 😕",
+    'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
+}
+
+HELP_TXT = """**🌟 Help Menu** 
+
+**__FOR PUBLIC CHATS__**
+
+```
+Just Copy The Public Restricted Post Link And Send Me To See The Magic ✨
+```
+
+**__FOR PRIVATE CHATS__**
+
+```
+Sorry! Currently Not Available 😕
+```
+
+**__FOR BOT CHATS__**
+
+```
+Will Be Added Soon...
+```
+**__FOR SAVE IN BULK__**
+```
+Just Click On 👇
+```
+<blockquote>/batch</blockquote>
+
+**𝗡𝗢𝗪 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗔𝗕𝗨𝗦𝗘 𝗠𝗘 🫣**"""
+
+BATCH_TXT = """𝐎𝐡𝐡! 𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫 𝐈𝐟 𝐘𝐨𝐮 𝐒𝐚𝐲 𝐈 𝐖𝐢𝐥𝐥 𝐆𝐢𝐯𝐞 𝐘𝐨𝐮 𝐌𝐲 𝐕𝐢𝐫𝐠𝐢𝐧𝐢𝐭𝐲 𝐁𝐮𝐭 𝐈 𝐂𝐚𝐧'𝐭 𝐏𝐫𝐨𝐜𝐞𝐞𝐝 𝐓𝐡𝐢𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 🥺
+
+𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐨𝐫𝐠𝐢𝐯𝐞 𝐌𝐞 🙏
+
+<blockquote>𝐈 𝐀𝐦 𝐓𝐫𝐲𝐢𝐧𝐠 𝐌𝐲 𝐁𝐞𝐬𝐭 𝐓𝐨 𝐒𝐚𝐭𝐢𝐬𝐟𝐲 𝐘𝐨𝐮 😘</blockquote>
+"""
+       
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
