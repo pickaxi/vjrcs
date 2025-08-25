32b3ec43f2f2165e157f163db2b04d68a260a659
Sam <freestff0077@gmail.com>
2025-08-26 00:14:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8272818476:AAHIsbGS1Dt7c2aQNDmYyVYqqJVm5deqnHk")
 API_ID = int(os.environ.get("API_ID", "28735699"))
 API_HASH = os.environ.get("API_HASH", "2e19c326d8cb322df7c15d7b7e84d1f3")
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://fixmayart834:FMWwXBd4JJYMs2Iv@cluster0.ltpube9.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://ridhatlog:RcZ4EVi6hzwSaarZ@cluster0.dxj0ntg.mongodb.net/?retryWrites=true&w=majority")
+
 ADMINS = list(map(int, os.environ.get("ADMINS", "6573328336 7794867502").split()))
 #DUMP = int(os.environ.get("DUMP", "-1002867430238"))
 WATERMARK_TEXT = os.environ.get("WATERMARK_TEXT", "Ankit")
