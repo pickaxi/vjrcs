ed9a8facff8d57ad85ea316fcfef1d1bccaf4c08
Sam <freestff0077@gmail.com>
2025-08-25 21:55:07 +0530

    Create dump.py

---
+++ TechVJ/dump.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import ADMINS
+from database.db import dump_collection
+
+def is_admin(user_id):
+    admins = [ADMINS] if isinstance(ADMINS, int) else ADMINS
+    return user_id in admins
+
+@Client.on_message(filters.command(["add"]) & filters.private)
+async def add_dump(client: Client, message: Message):
+    if not is_admin(message.from_user.id):
+        await message.reply("Only admins can set DUMP channel.")
+        return
+    args = message.text.split()
+    if len(args) != 2:
+        await message.reply("Usage: /add <channel_id>")
+        return
+    channel_id = args[1]
+    dump_collection.delete_many({})  # Only one DUMP channel at a time
+    dump_collection.insert_one({"channel_id": channel_id})
+    await message.reply(f"DUMP channel set to `{channel_id}`.")
+
+@Client.on_message(filters.command(["dl"]) & filters.private)
+async def delete_dump(client: Client, message: Message):
+    if not is_admin(message.from_user.id):
+        await message.reply("Only admins can delete DUMP channel.")
+        return
+    dump_collection.delete_many({})
+    await message.reply("DUMP channel removed.")
+
+def get_dump_channel():
+    dump = dump_collection.find_one({})
+    return int(dump["channel_id"]) if dump else None
