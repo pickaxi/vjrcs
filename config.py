2226e4e07a7906a77bb6e808036f1011a8ab9fdc
Sam <freestff0077@gmail.com>
2025-08-26 00:20:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8272818476:AAHIsbGS1Dt7c2aQNDmYyVYqqJVm5deqnHk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7726282640:AAGSWtdGlOoRAlVDDMx_fJhX20BZAtfguD8")
 API_ID = int(os.environ.get("API_ID", "28735699"))
 API_HASH = os.environ.get("API_HASH", "2e19c326d8cb322df7c15d7b7e84d1f3")
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://ridhatlog:RcZ4EVi6hzwSaarZ@cluster0.dxj0ntg.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://rotbut457:bAL9JXScaytVmSCX@cluster0.p6uxmcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 ADMINS = list(map(int, os.environ.get("ADMINS", "6573328336 7794867502").split()))
 #DUMP = int(os.environ.get("DUMP", "-1002867430238"))
