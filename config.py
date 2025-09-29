bbeb2d424efb264600e68c5f74aae087b10bf66f
patrioremrej <patrioremrej@gmail.com>
2025-09-29 20:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7726282640:AAGSWtdGlOoRAlVDDMx_fJhX20BZAtfguD8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8246933872:AAEW7FWLDdoBmLAgeSwpkZ9WcyriRx--wpI")
 API_ID = int(os.environ.get("API_ID", "28735699"))
 API_HASH = os.environ.get("API_HASH", "2e19c326d8cb322df7c15d7b7e84d1f3")
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://ridhatlog:RcZ4EVi6hzwSaarZ@cluster0.dxj0ntg.mongodb.net/?retryWrites=true&w=majority")
 
-ADMINS = list(map(int, os.environ.get("ADMINS", "6573328336").split()))
+ADMINS = list(map(int, os.environ.get("ADMINS", "5104279187 6573328336").split()))
+
 #DUMP = int(os.environ.get("DUMP", "-1002867430238"))
 WATERMARK_TEXT = os.environ.get("WATERMARK_TEXT", "Ankit")
 SPLIT_SIZE = os.environ.get("SPLIT_SIZE", 2000 * 1024 * 1024)
